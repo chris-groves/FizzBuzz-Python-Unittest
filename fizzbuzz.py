6eed9b8f571928ef278fbde088c03cdc08e8e783
Chris Groves <chris_groves87@hotmail.co.uk>
2019-09-13 15:36:15 +0100

    feat: return fizzbuzz for 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,7 @@
 def fizzbuzz(number):
-    if number % 3 == 0:
+    if number % 3 == 0 and number % 5 == 0:
+      return 'Fizzbuzz'
+    elif number % 3 == 0:
       return 'Fizz'
     elif number % 5 == 0:
       return 'Buzz'
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -23,6 +23,10 @@ class TestFizzbuzz(unittest.TestCase):
       result = fizzbuzz.fizzbuzz(10)
       self.assertEqual(result, 'Buzz')
 
+  def test_return_fizzbuzz_for_15(self):
+      result = fizzbuzz.fizzbuzz(15)
+      self.assertEqual(result, 'Fizzbuzz')
+
   def test_return_number_for_7(self):
       result = fizzbuzz.fizzbuzz(7)
       self.assertEqual(result, 7)
