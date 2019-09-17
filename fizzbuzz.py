9c7a42f9620c7fd2b9db0c4d9247a04389d6d974
Chris Groves <chris_groves87@hotmail.co.uk>
2019-09-17 15:19:15 +0100

    feat: introduce loop to print 1-100, refactor: create class

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,17 +1,19 @@
-def fizzbuzz(number):
-    if number % 3 == 0 and number % 5 == 0:
-      return 'Fizzbuzz'
-    elif number % 3 == 0:
-      return 'Fizz'
-    elif number % 5 == 0:
-      return 'Buzz'
-    else:
-      return number
+class Fizzbuzz:
+    def __init__(self):
+        self.array = []
 
-def array():
-    x = 1
-    array = []
-    while x < 101:
-      array.append(x)
-      x += 1
-    return array[0]
+    def fizzbuzz_number(self, number):
+      if number % 3 == 0 and number % 5 == 0:
+        return 'Fizzbuzz'
+      elif number % 3 == 0:
+        return 'Fizz'
+      elif number % 5 == 0:
+        return 'Buzz'
+      else:
+        return number
+
+    def loop(self):
+      x = 1
+      while x < 101:
+        self.array.append(x)
+        x += 1
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,43 +1,53 @@
 import unittest
-import fizzbuzz
+from fizzbuzz import Fizzbuzz
 
 class TestFizzbuzz(unittest.TestCase):
 
   def test_return_number_1(self):
-        result = fizzbuzz.fizzbuzz(1)
+        fizzbuzz = Fizzbuzz()
+        result = fizzbuzz.fizzbuzz_number(1)
         self.assertEqual(result,1)
 
   def test_return_fizz_for_number_3(self):
-        result = fizzbuzz.fizzbuzz(3)
+        fizzbuzz = Fizzbuzz()
+        result = fizzbuzz.fizzbuzz_number(3)
         self.assertEqual(result,'Fizz')
 
   def test_return_fizz_for_multiple_of_3(self):
-        result = fizzbuzz.fizzbuzz(9)
+        fizzbuzz = Fizzbuzz()
+        result = fizzbuzz.fizzbuzz_number(9)
         self.assertEqual(result, 'Fizz')
 
   def test_return_buzz_for_number_5(self):
-       result = fizzbuzz.fizzbuzz(5)
+       fizzbuzz = Fizzbuzz()
+       result = fizzbuzz.fizzbuzz_number(5)
        self.assertEqual(result, 'Buzz')
 
   def test_return_buzz_for_multiple_of_5(self):
-      result = fizzbuzz.fizzbuzz(10)
+      fizzbuzz = Fizzbuzz()
+      result = fizzbuzz.fizzbuzz_number(10)
       self.assertEqual(result, 'Buzz')
 
   def test_return_fizzbuzz_for_15(self):
-      result = fizzbuzz.fizzbuzz(15)
+      fizzbuzz = Fizzbuzz()
+      result = fizzbuzz.fizzbuzz_number(15)
       self.assertEqual(result, 'Fizzbuzz')
 
   def test_return_fizzbuzz_for_30(self):
-      result = fizzbuzz.fizzbuzz(30)
+      fizzbuzz = Fizzbuzz()
+      result = fizzbuzz.fizzbuzz_number(30)
       self.assertEqual(result, 'Fizzbuzz')
 
   def test_return_number_for_7(self):
-      result = fizzbuzz.fizzbuzz(7)
+      fizzbuzz = Fizzbuzz()
+      result = fizzbuzz.fizzbuzz_number(7)
       self.assertEqual(result, 7)
 
   def test_first_array_element_1(self):
-      result = fizzbuzz.array()
+      fizzbuzz = Fizzbuzz()
+      fizzbuzz.loop()
+      result = fizzbuzz.array[0]
       self.assertEqual(result, 1)
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     unittest.main()
